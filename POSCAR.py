b17f338bd25a5a13b07d535dc9565131fa9ea7ab
Woosun Jang <jin890@yonsei.ac.kr>
2018-07-31 12:02:06 +0900

    PEP8 style formatting

--- POSCAR.py
+++ POSCAR.py
@@ -1,10 +1,11 @@
-from __future__ import division , print_function
-from collections import OrderedDict
-from collections import deque
-import numpy as np
+from __future__ import division, print_function
+from collections import OrderedDict, deque
 from B_C_M import getDistance
+import numpy as np
+import itertools
 
-class fileWriter:
+
+class FileWriter:
     def __init__(self, file):
         self.file = file
 
@@ -16,17 +17,17 @@ class fileWriter:
 
 
 class POSCAR:
-    def __init__(self, atoms, coord, lattice, name, I, S, Smatrix):
+    def __init__(self, atoms, coord, lattice, name, scaling, selective, s_matrix):
         self.atoms = atoms
         self.coord = coord
         self.lattice = lattice
         self.type = 'Cartesian'
         self.name = name
-        self.I = I
-        self.S = S
-        self.Smatrix = Smatrix
+        self.scaling = scaling
+        self.selective = selective
+        self.s_matrix = s_matrix
         self.ATOMS = OrderedDict()
-        self.organize_by_Atom()
+        self.organize_by_atom()
 
         self.index_to_atom = {}
         self.atom_to_index = {}
@@ -34,29 +35,29 @@ class POSCAR:
             self.index_to_atom[idx] = atm
             self.atom_to_index[atm] = idx
 
-    def organize_by_Atom(self):
+    def organize_by_atom(self):
         idx, idx2 = 0, 0
         for atom, num in self.atoms.items():
             idx += idx2
             idx2 += num
             self.ATOMS[atom] = self.coord[idx:idx2]
 
-    def print_POSCAR(self, filename):
+    def print_poscar(self, filename):
         file = open(filename, 'w')
-        f = fileWriter(file)
+        f = FileWriter(file)
         f.write(self.name)
-        f.write(self.I)
+        f.write(self.scaling)
         for i in self.lattice:
             f.write(" ".join(map(str, i)))
         f.write(" ".join(self.atoms.keys()))
         f.write(" ".join(map(str, self.atoms.values())))
-        if self.S:
+        if self.selective:
             f.write("Selective Dynamics")
         f.write(self.type)
 
-        if self.S:
+        if self.selective:
             for i in range(len(self.coord)):
-                f.write(" ".join(map(str, list(self.coord[i]) + self.Smatrix[i])))
+                f.write(" ".join(map(str, list(self.coord[i]) + self.s_matrix[i])))
         else:
             for i in self.coord:
                 f.write(" ".join(map(str, i)))
@@ -64,7 +65,7 @@ class POSCAR:
         f.close()
 
     def get_max_height_of_atom(self, atom):
-        ## matrix
+        # matrix
         return max([i[2] for i in self.ATOMS[atom]])
 
     def update_atom(self, atom):
@@ -79,16 +80,16 @@ class POSCAR:
             idx += 1
 
 
-def post_process(line_list, Selective, Direct, l_parameter):
+def post_process(line_list, selective, direct, l_parameter):
     coord = deque()
-    sel = deque() if Selective else None
-    if Selective:
+    sel = deque() if selective else None
+    if selective:
         for line in line_list:
             tmp = line.split()
             tmp_coord = np.array(list(map(float, tmp[:3])))
             if len(tmp_coord) == 0:
                 break
-            if Direct:
+            if direct:
 
                 coord.append(list(np.matmul(tmp_coord, l_parameter)))
             else:
@@ -99,7 +100,7 @@ def post_process(line_list, Selective, Direct, l_parameter):
             tmp_coord = np.array(list(map(float, line.split())))
             if len(tmp_coord) == 0:
                 break
-            if Direct:
+            if direct:
 
                 coord.append(list(np.matmul(tmp_coord, l_parameter)))
             else:
@@ -107,10 +108,10 @@ def post_process(line_list, Selective, Direct, l_parameter):
     return coord, sel
 
 
-def readPOSCAR(file):
+def read_poscar(file):
     line_list = file.read().splitlines()
     title = line_list[0]
-    I = line_list[1]
+    scaling = line_list[1]
     lattice = [list(map(float, i.split())) for i in line_list[2:5]]
 
     l_parameter = np.array(lattice)
@@ -118,29 +119,31 @@ def readPOSCAR(file):
     tmp = line_list[5].split(), list(map(int, line_list[6].split()))
     atoms = OrderedDict((tmp[0][i], tmp[1][i]) for i in range(len(tmp[0])))
 
-    Selective = False
+    selective = False
     if line_list[7].upper()[0] == 'S':
-        Selective = True
-    idx = 8 if Selective else 7
-    Direct = True if line_list[idx].upper()[0] == 'D' else False
+        selective = True
+    idx = 8 if selective else 7
+    direct = True if line_list[idx].upper()[0] == 'D' else False
 
-    coord, sel = post_process(line_list[idx + 1:], Selective, Direct, l_parameter)
+    coord, sel = post_process(line_list[idx + 1:], selective, direct, l_parameter)
     coord = [i for i in coord]
-    SelMatrix = [i for i in sel] if Selective else sel
+    sel_matrix = [i for i in sel] if selective else sel
     file.close()
-    return POSCAR(atoms, coord, lattice, title, I, Selective, SelMatrix)
-def reArrange(Atoms1, Atoms2):
-    B = np.zeros((len(Atoms1),len(Atoms1)))
-    tmp = [0 for i in range(len(Atoms1))]
-    for i in itertools.product(range(len(Atoms1)),repeat=2):
-        B[i] = getDistance(np.array(Atoms1[i[0]]),np.array( Atoms2[i[1]]))
+    return POSCAR(atoms, coord, lattice, title, scaling, selective, sel_matrix)
 
-    for _ in range(len(Atoms1)):
-        x,y = np.unravel_index(B.argmin(), B.shape)
+
+def rearrange(atoms1, atoms2):
+    distance_matrix = np.zeros((len(atoms1), len(atoms1)))
+    tmp = [0 for i in range(len(atoms1))]
+    for i in itertools.product(range(len(atoms1)), repeat=2):
+        distance_matrix[i] = getDistance(np.array(atoms1[i[0]]), np.array(atoms2[i[1]]))
+
+    for _ in range(len(atoms1)):
+        x, y = np.unravel_index(distance_matrix.argmin(), distance_matrix.shape)
         tmp[y] = x
-        B[x] = np.full(len(Atoms1), np.inf)
-        B[:,y] =np.full(len(Atoms1),np.inf)
-    newM = [None for i in range(len(Atoms1))]
-    for i,j in enumerate(tmp):
-        newM[i] = Atoms1[j]
-    return newM
+        distance_matrix[x] = np.full(len(atoms1), np.inf)
+        distance_matrix[:, y] = np.full(len(atoms1), np.inf)
+    new_m = [None for i in range(len(atoms1))]
+    for i, j in enumerate(tmp):
+        new_m[i] = atoms1[j]
+    return new_m
